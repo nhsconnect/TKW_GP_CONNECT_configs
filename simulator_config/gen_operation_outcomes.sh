22c7d129714e09545167b0586a955e6eec2e419d
sifa2 <simon.farrow1@nhs.net>
2020-04-23 09:59:51 +0100

    First cut with Documents

--- simulator_config/gen_operation_outcomes.sh
+++ simulator_config/gen_operation_outcomes.sh
@@ -5,8 +5,10 @@ do
 	if [[ ! $tag =~ ^#.*$ ]]
 	then
 		echo $tag
+		# convert BAD_REQUEST to Bad Request
+		detail=`echo $code | sed 's/_/ /g' | sed -r 's/([^ ])([^ ]*)/\U\1\L\2/g'`
 		sed -e s/__ERRORCODE__/$code/ \
-			-e s/__ERRORDETAIL__/$code/ \
+			-e s/__ERRORDETAIL__/"$detail"/ \
 			-e s/__ERRORTEXT__/"$diagnostics"/ \
 			< operation_outcomes/operation_outcome.xml  > operation_outcomes/operation_outcome_$tag.xml
 	fi
