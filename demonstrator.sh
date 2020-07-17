8b19304c0314f3fd5e43285ecc18c64a044c267d
Simon Farrow <simon.farrow1@nhs.net>
2020-07-17 13:56:13 +0100

    Moved some tkw props files to legacy_files.

--- demonstrator.sh
+++ demonstrator.sh
@@ -9,16 +9,23 @@ ROOT=~/Documents/git_repositories
 case $1 in 
 	0.7)
 		cd $ROOT/gpconnect-demonstrator-release-0.7
+		./start_gpconnect.sh
 		;;
 	1.2)
 		cd $ROOT/gpconnect-demonstrator-release-1.2
+		docker-compose -f gpconnect.yml up
 		;;
-	1.3)
+	1.2-opentest)
+		cd $ROOT/gpconnect-demonstrator-release-1.2
+		docker-compose -f gpconnect-opentest.yml up
+		;;
+	1.5)
 		cd $ROOT/gpconnect-demonstrator-develop
+		docker-compose -f gpconnect.yml up
 		;;
 	*)
 		cd $ROOT/gpconnect-demonstrator-release-0.5.0
+		./start_gpconnect.sh
 		;;
 esac
 
-./start_gpconnect.sh
