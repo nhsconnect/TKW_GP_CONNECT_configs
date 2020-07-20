7afb59acfd60ccca844c57aabb02fb46ba21ae20
Simon Farrow <simon.farrow1@nhs.net>
2020-07-20 12:49:08 +0100

    New opentest certs, refactored simulator.sh.

--- simulator.sh
+++ simulator.sh
@@ -7,33 +7,31 @@ JAVA_OPTIONS=-Dtks.skipsignlogs=Y
 #
 # Test tkw properties injection
 #JAVA_OPTIONS+=" -Dtks.serverBaseUrl=xxxx"
+#JAVA_OPTIONS+=" -Djavax.net.debug=all"
 
 #MODE=-simulator
 MODE=-httpinterceptor
 
 TEMP=temp
 
+# This file is used as a template but the ensuing default edits for the F option make it identical to the forwarder props file
+PROPS=tkw-x.properties
+
 case $1 in
 	F)
 	# forward all
-	#PROPS=tkw_forward_all.properties
-	PROPS=tkw-x.properties
 	;;
 
-	SF)
+	SF|FS)
 	# secure forward
-	PROPS=tkw_forward_all_secure.properties
 	;;
 
 	S)
 	# secure simulator
-	PROPS=tkw_secure.properties
 	;;
 
 	C)
 	# default simulate some
-	PROPS=tkw-x.properties
-	#PROPS=tkw.properties
 	;;
 
 	*)
@@ -46,24 +44,47 @@ esac
 cp $PROPS $TEMP
 PROPS=$TEMP
 
+#===================================================================================
+# forwarding port
+
+# default value
 FORWARDING_PORT=5000
 if [[ "$3" != "" ]]
 then
+	# replace default
 	sed -i -e "/tks\.httpinterceptor\.forwardingport/s/$FORWARDING_PORT/$3/" $TEMP
 	FORWARDING_PORT=$3
 fi
 
-if [[ "$1" == "F" ]]
+#===================================================================================
+# mode
+
+# forward
+if [[ "$1" =~ ^.*F.*$  ]]
 then
+	# modify rules file and forwarding port
 	sed -i -e "/tks\.rules\.configuration\.file/s/test_tks_rule_config.txt/test_tks_rule_forwarder_config.txt/" $TEMP
 	sed -i -e "/tks\.configname/s/FHIR_GPCONNECT/FHIR_GPCONNECT forwarding all ==> $FORWARDING_PORT/" $TEMP
 fi
 
+# secure
+if [[ "$1" =~ ^.*S.*$ ]]
+then
+	# modify TLS setting
+	sed -i -e "/tks\.receivetls/s/No/Yes/" $TEMP
+	sed -i -e "/tks\.configname/s/$/ secure/" $TEMP
+fi
+
+#===================================================================================
+# listen port
+
 if [[ "$2" != "" ]]
 then
+	# modify listen port
 	sed -i -e "/HttpTransport\.listenport/s/4848/$2/" $TEMP
 fi
 
+#===================================================================================
 
 #$JDK8/bin/java $JAVA_OPTIONS -jar ../../TKW.jar $MODE $PROPS
 $JDK11/bin/java $JAVA_OPTIONS -jar ../../TKW-x.jar $MODE $PROPS
