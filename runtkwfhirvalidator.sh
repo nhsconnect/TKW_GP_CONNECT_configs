e75cca82ad02fc65415356776d00642fe1bac1e4
Simon Farrow <simon.farrow1@nhs.net>
2021-06-16 13:27:47 +0100

    Added support for building docker image

--- runtkwfhirvalidator.sh
+++ runtkwfhirvalidator.sh
@@ -11,6 +11,9 @@ then
 	exit 0
 fi
 
+forwarding_address=gpconnect-api
+forwarding_port=19191
+
 echo "Running as UID $UID"
 echo "trustStore = " $trustStore
 echo "trustStorePassword = " $trustStorePassword
@@ -22,14 +25,22 @@ tar -xvf /home/service/TKW/config/$PROJECT/tkwoutputstructure.tar
 cd /home/service
 # decide whether its TLSMA or not
 
+# wait until the gpconnect demonstrator docker is up before starting
+/home/service/TKW/config/$PROJECT/wait-for-it.sh -t 0 $forwarding_address:$forwarding_port
 
-props_file=tkw-x-forwarding.properties
+props_file=/home/service/TKW/config/$PROJECT/tkw-x-forwarding.properties
+
+sed -i -e /^tks.httpinterceptor.forwardingaddress/s/127.0.0.1/$forwarding_address/g $props_file
+sed -i -e /^tks.httpinterceptor.forwardingport/s/5000/$forwarding_port/g $props_file
+sed -i -e /^tks.configname/s/127.0.0.1/$forwarding_address/g $props_file
+sed -i -e /^tks.configname/s/5000/$forwarding_port/g $props_file
+
 if [ "$trustStore" == 'default' ]
 then
 	#ClearText
 	java -version
-	java -XX:+UseContainerSupport -XX:MaxRAMPercentage=80.0 -jar /home/service/TKW/TKW-x.jar -httpinterceptor /home/service/TKW/config/$PROJECT/$props_file
+	java -XX:+UseContainerSupport -XX:MaxRAMPercentage=80.0 -jar /home/service/TKW/TKW-x.jar -httpinterceptor $props_file
 else
 	#TLSMA
-	java -Djavax.net.ssl.trustStore=$trustStore -Djavax.net.ssl.trustStorePassword=$trustStorePassword -Djavax.net.ssl.keyStore=$keyStore -Djavax.net.ssl.keyStorePassword=$keyStorePassword -jar /home/service/TKW/TKW-x.jar -httpinterceptor /home/service/TKW/config/$PROJECT/$props_file
+	java -Djavax.net.ssl.trustStore=$trustStore -Djavax.net.ssl.trustStorePassword=$trustStorePassword -Djavax.net.ssl.keyStore=$keyStore -Djavax.net.ssl.keyStorePassword=$keyStorePassword -jar /home/service/TKW/TKW-x.jar -httpinterceptor $props_file
 fi
